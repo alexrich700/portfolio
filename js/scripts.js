112eb314b4fc3d88d91fe9f618e108537c39deee
unknown <alexrich700@gmail.com>
2019-10-23 23:05:43 -0500

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -56,7 +56,7 @@ function initBionick() {
     });
 	$(".project-pagination , .fixed-filter , .searh-holder").addClass("in-anim");
     $(".wrapper-inner , .fixed-column , footer , .hero-wrapper").addClass("viselem");
-    $("nav li.subnav a").append('<i class="fa fa-angle-double-down subnavicon"></i>');
+    // $("nav li.subnav a").append('<i class="fa fa-angle-double-down subnavicon"></i>');
 	// magnificPopup ------------------
     $(".image-popup").magnificPopup({
         type: "image",
@@ -489,4 +489,40 @@ function initparallax() {
 $(document).ready(function() {
     initBionick();
     initparallax();
+});
+
+// Animated text
+
+function autoType(elementClass, typingSpeed){
+  var thhis = $(elementClass);
+  thhis.css({
+    "position": "relative",
+    "display": "inline-block"
+  });
+  thhis.prepend('<div class="cursor" style="right: initial; left:0;"></div>');
+  thhis = thhis.find(".text-js");
+  var text = thhis.text().trim().split('');
+  var amntOfChars = text.length;
+  var newString = "";
+  thhis.text("|");
+  setTimeout(function(){
+    thhis.css("opacity",1);
+    thhis.prev().removeAttr("style");
+    thhis.text("");
+    for(var i = 0; i < amntOfChars; i++){
+      (function(i,char){
+        setTimeout(function() {        
+          newString += char;
+          thhis.text(newString);
+        },i*typingSpeed);
+      })(i+1,text[i]);
+    }
+  },1500);
+}
+
+$(document).ready(function(){
+  // Now to start autoTyping just call the autoType function with the 
+  // class of outer div
+  // The second paramter is the speed between each letter is typed.   
+  autoType(".type-js",200);
 });
